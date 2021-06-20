a6a653d2390a900230395d25f94d553063bf9f7f
Rizwan Khot <khotrizwan@gmail.com>
2021-06-21 01:11:12 +0530

    Angular Event Binding Example done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-event-binding';
+  title = 'Angular Event Binding Example';
+  count = 0;
+  allowRestart = false;
+  constructor() { 
+  }
+
+  ngOnInit() {
+    console.log(this.title);
+    
+      setTimeout(()=>{                           //<<<---using ()=> syntax
+        this.countFn(this.count + 1)
+      }, 1000);
+    
+  }
+
+  countFn (count: any): any {
+    this.count = count;
+    console.log(count);
+    if(count < 10) {
+      setTimeout(()=>{                           //<<<---using ()=> syntax
+        this.countFn(this.count + 1)
+      }, 1000);
+    } else {
+      this.allowRestart = true;
+    }
+  }
+
+  restart() {
+    this.allowRestart = false;
+    this.count = 0;
+    setTimeout(()=>{                           //<<<---using ()=> syntax
+      this.countFn(this.count + 1)
+    }, 1000);
+  }
 }
